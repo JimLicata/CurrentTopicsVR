fe7f406bc473c99e72d9fd6672cac63b2cfe7352
James Licata <jimmays.jl@gmail.com>
2019-01-28 04:08:00 -0500

    initial

---
+++ Assets/SpinMe.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinMe : MonoBehaviour {
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+    public void Spin()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        rb.AddRelativeTorque(new Vector3(0.0f, 10.0f), ForceMode.Impulse);
+    }
+}
